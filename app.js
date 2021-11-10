964cd8381f56028b529765fe565804cbaa82caad
mohsin-8 <mohsinalikhan518@gmail.com>
2021-11-10 22:56:35 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+// var userNum = prompt("Enter your positive number");
+
+// // var number = Math.round(userNum);
+// // var number = Math.ceil(userNum);
+// // var number = Math.floor(userNum);
+
+
+// console.log(number);
+
+
+
+// var userNum = prompt("Enter your positive number");
+
+// // var number = Math.round(userNum);
+// // var number = Math.ceil(userNum);
+// var number = Math.floor(userNum);
+
+
+// console.log(number);
